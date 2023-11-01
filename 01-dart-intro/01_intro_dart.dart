09e73875cb23d4e620338525d2966270d4872a5a
desarrollolc <lucianonicolas10@gmail.com>
2023-11-01 12:06:04 -0300

    new course started

---
+++ 01-dart-intro/01_intro_dart.dart
@@ -0,0 +1,9 @@
+void main(){
+  // var myName = 'Luciano'
+  // String myName = 'Luciano Cerullo';
+  // late final myName = 'Nicolas';
+  const myName = 'papu';
+  
+  
+  print('Hola ${myName}');
+}
