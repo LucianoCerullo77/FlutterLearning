d8e617ce183f7162d6b8450b284cbd81fe3e0de2
desarrollolc <lucianonicolas10@gmail.com>
2023-11-05 17:30:17 -0300

    variables types created

---
+++ 01-dart-intro/02-variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  final String pokemon = 'Ditto';
+  final int hp = 100;
+  final bool isAlive = true;
+  // final abilities = <String>['impostor'];
+  final list<String> abilities = <String>['impostor'];
+  final sprites = <String>['ditto/front.png','ditto/back.png'];
+
+  print("""
+
+  $pokemon
+  $hp
+  $isAlive
+
+
+  """);
+
+}
