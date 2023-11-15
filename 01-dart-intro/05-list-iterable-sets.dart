d557dcc3201d34ed3e8484a05fe2ae4ce70c3d8e
desarrollolc <lucianonicolas10@gmail.com>
2023-11-14 23:45:52 -0300

    testing iterables with reversed numbers

---
+++ 01-dart-intro/05-list-iterable-sets.dart
@@ -0,0 +1,18 @@
+void main() {
+  final numbers = [1,2,3,3,4,5,6,5,2,3,5,7,98,19];
+
+  print('List Original $numbers');
+
+  print('Length ${numbers.length}');
+
+  print('Index ${numbers[0]}');
+
+  print('First ${numbers.first}');
+
+  print('Last ${numbers.last}');
+  
+  print('Reversed ${numbers.reversed}');
+
+  final reversedNumbers = numbers.reversed;
+
+}
