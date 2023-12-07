1b273019a90afb0b1d2eed31f2db53b31f65dffb
desarrollolc <lucianonicolas10@gmail.com>
2023-12-06 22:02:41 -0300

    async await updated

--- 01-dart-intro/18-async-await.dart
+++ 01-dart-intro/18-async-await.dart
@@ -2,14 +2,25 @@ void main () async {
 
   print('Start');
 
+  try{
+
   final value = await httpGet('https://fernando-herrera.com/cursos');
 
   print(value);
+
+  } catch (err) {
+    print('Error found : $err')
+  }
+
+
   print('End');
 }
 
 Future<String> httpGet(String url) async {
 
   await Future.delayed( const Duration(seconds: 1));
-  return 'HTTP Value !!!'
+
+  throw 'Petition Error'
+
+  // return 'HTTP Value !!!'
 }
