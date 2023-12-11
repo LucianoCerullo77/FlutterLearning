b65c0bd19d40b1c7f884dd3c405e0d2ccdd6b8f2
desarrollolc <lucianonicolas10@gmail.com>
2023-12-11 11:46:43 -0300

    adding new flutter project as a test

--- 01-dart-intro/02-variables.dart
+++ 01-dart-intro/02-variables.dart
@@ -3,7 +3,7 @@ void main() {
   final int hp = 100;
   final bool isAlive = true;
   // final abilities = <String>['impostor'];
-  final list<String> abilities = <String>['impostor'];
+  final ist<String> abilities = <String>['impostor'];
   final sprites = <String>['ditto/front.png','ditto/back.png'];
 
   print("""
