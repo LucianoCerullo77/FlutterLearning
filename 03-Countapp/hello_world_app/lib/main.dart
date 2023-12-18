8b7ae43d8fd36c459a8ae46debb99cd4a69df719
desarrollolc <lucianonicolas10@gmail.com>
2023-12-18 11:39:44 -0300

    adding good practices to the current code, commented so i cant forget it

--- 03-Countapp/hello_world_app/lib/main.dart
+++ 03-Countapp/hello_world_app/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp( const MyApp() );
+  // setting the const to remove error lens on code.
 }
 
 
 class MyApp extends StatelessWidget {
 
+  const MyApp({super.key});
+  // good practice, key applied
+
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
+   return const MaterialApp(
+    // another good practice, setting the const to the widget.
+
+    debugShowCheckedModeBanner: false,
+    // removing the DEBUG banner on the phone / virtual device.
+
     home: Scaffold(
       body:  Center(child: Text('Hello World')),
     )
