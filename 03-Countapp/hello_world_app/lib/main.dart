ff76192c566f7c070b815eb046a4d255068aed37
Luciano <lucianonicolas10@gmail.com>
2023-12-21 22:08:49 -0300

    addind packages

--- 03-Countapp/hello_world_app/lib/main.dart
+++ 03-Countapp/hello_world_app/lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-    home: Center(child: Text('Hello World'))
-   );
+    return MaterialApp(home: Center(child: Text('Hello World')));
   }
-
 }
