1d638739a6f9cdab82260550b92a8d1a2c374edb
Luciano <lucianonicolas10@gmail.com>
2023-12-24 12:33:18 -0300

    adding logic to counter text with if functions, if is equal to 1, then the text will be click, if is not, then the text will say clicks!

--- 03-Countapp/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ 03-Countapp/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -11,6 +11,7 @@ class _CounterScreenState extends State<CounterScreen> {
   int clickCounter = 0;
 
   String screenTitle = "Counter Screen";
+  String counterText = "Click";
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +30,8 @@ class _CounterScreenState extends State<CounterScreen> {
               style:
                   const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            const Text('Clicks',
-                style: TextStyle(
+            Text(counterText,
+                style: const TextStyle(
                   fontSize: 25,
                 ))
           ],
@@ -39,7 +40,12 @@ class _CounterScreenState extends State<CounterScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // we write all the method here
-          clickCounter ++;
+          clickCounter++;
+
+          // adding new method, if clickCounter is equal to 1, then the counterText will say click, if is more than 1, it will say clicks!
+          if (clickCounter != 1 ) {
+            counterText = "Clicks";
+          }
 
           // then we render again, calling the state.
           setState(() {});
